b9b7e0464d8f377763df6b0563d74ea261837afb
palmerjoshua <palmerjoshua2013@fau.edu>
2016-03-21 17:30:15 -0400

    converted to single file to satisfy submission requirements

--- palmerjoshua2013_PyHomework1.py
+++ palmerjoshua2013_PyHomework1.py
@@ -11,8 +11,35 @@ This script runs the entire homework assignment plus the extra credit.
 - Some helper functions were created to make the program's output a little nicer to read.
 """
 
-from palmerjoshua2013_helpers import pp, make_pretty
 
+# ============================= #
+#            HELPERS            #
+# ============================= #
+import pprint
+pp = pprint.PrettyPrinter(indent=4)  # makes dictionaries look nicer when printed to the console
+
+
+def make_pretty(title):
+    """This decorator is used to print a header and footer around my output. Instead of littering every
+       function with the same prints, prepending them with this decorator will automatically add them for me.
+       :param title - The title of the section that will be printed. Can be any string."""
+
+    length = 20  # length of each side of the header
+    symbol = "#"  # the symbol used to print the header
+    bottom_length = 2 + 2*length + len(title)  # length of the footer
+
+    def decorator(func):
+        def wrapper():
+            print(symbol*length, title, symbol*length)  # print header
+            func()  # call original function
+            print(symbol*bottom_length)  # print footer
+            print()  # print blank line
+        return wrapper
+    return decorator
+
+# ============================= #
+#        MAIN ASSIGNMENT        #
+# ============================= #
 classes = {}
 '''I made this global because I have to print its contents long after the function that mutates it has finished executing.
 There's no good reason to make this variable global other than making it more convenient to create/mutate it in problem #9-11
@@ -47,8 +74,7 @@ def container_stuff():  # questions 9-11
                "Data Communications": "Mahgoub",
                "Programming Languages": "Huang"}
 
-    print_fn= pp.pprint if pp else print
-    print_fn(classes)
+    pp.pprint(classes)
     print(favorite)
 
 
@@ -115,10 +141,56 @@ def print_classes():  # questions 20-21
     '''
 
 
+# ============================= #
+#          EXTRA CREDIT         #
+# ============================= #
+class Student:
+    count = 0  # static variable to count number of Student instances
+
+    def __init__(self, name="", age=0, birth_month=""):
+        """Create student data, either from parameters or by getting keyboard input from the user.
+        :param name - name of the student. If omitted, the user will be asked to provide a value using the keyboard.
+        :param age - age of the student; if omitted, the user will be asked to provide a value using the keyboard.
+        :param birth_month - birth month of the student. If omitted, the user will be asked to provide a value using the keyboard.
+        """
+        Student.count += 1
+        self.name = name or input("Enter name for student #{}: ".format(Student.count))
+        self.age = age or Student._get_age()
+        self.birth_month = birth_month or input("Enter birth month for student #{}: ".format(Student.count))
+
+    def display_name(self):
+        """Although the homework asks us to use the word 'display' in our function names, the homework also says the functions
+        should just return the values and not print the values or display them in any other way."""
+        return self.name
+
+    def display_birth_month(self):
+        return self.birth_month
+
+    @staticmethod
+    def _get_age():
+        """Used to get an integer from keyboard input. If the input cannot be cast to an integer, the user is asked to re-enter
+        the input until it can be cast to an integer."""
+        age = None
+        while not age:
+            try:
+                age = int(input("Enter age for student #{}: ".format(Student.count)))
+            except ValueError:
+                print("Invalid value for student age. Please enter an integer.")
+        return age
+
+
 @make_pretty("STUDENT DEMO")
-def student_demo():  # extra credit
-    from palmerjoshua2013_student import do_demo
-    do_demo()
+def student_demo():
+    student_data = [{"name": "Josh", "age": 25, "birth_month": "June"},
+                    {"name": "John", "age": 24, "birth_month": "July"},
+                    {"name": "Jack", "age": 23, "birth_month": "August"}]
+
+    # pass the student data as keyword arguments, and create multiple students with a list comprehension
+    students = [Student(**student) for student in student_data]
+
+    print("Student 1 Name:", students[0].display_name())
+    print("Student 2 Month:", students[1].display_birth_month())
+    print("Student Count:", Student.count)
 
 
 def main():
